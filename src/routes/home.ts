f3431b59c1e1b49c98a8dae550283e44bba12a25
Nahid <nahidthedeveloper@gmail.com>
2025-07-07 03:51:29 +0600

    add token check

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
@@ -8,6 +9,24 @@ router.get('/', (req: Request, res: Response) => {
         version: '1.0.0',
         author: 'Mst Humaira',
     })
+})
+
+router.get('/check-token', (req: Request, res: Response) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '')
+
+    if (!token) {
+        res.status(401).json({ message: 'Unauthorized access' })
+        return
+    }
+
+    try {
+        jwt.verify(token, process.env.JWT_SECRET as string)
+        res.status(200).json({ message: 'Token is valid' })
+        return
+    } catch {
+        res.status(401).json({ message: 'Invalid token' })
+        return
+    }
 })
 
 export default router
